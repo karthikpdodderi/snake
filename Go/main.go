fc52e286c9f8452bd6a2e51bb5b9a87d655d1bbc
karthikpdodderi <karthik.p.dodderi@gmail.com>
2024-10-14 22:40:54 +0530

    minor changes in num cols and num rows

--- Go/main.go
+++ Go/main.go
@@ -7,6 +7,6 @@ import (
 )
 
 func main() {
-	logicInterface := logic.NewLogic(10*time.Millisecond, 20, 4, 4, 5, board.LEFT, '*', 'M', '.', 'q', 'w', 's', 'a', 'd', 1000*time.Millisecond, false)
+	logicInterface := logic.NewLogic(10*time.Millisecond, 20, 10, 10, 5, board.LEFT, '*', 'M', '.', 'q', 'w', 's', 'a', 'd', 300*time.Millisecond, false)
 	logicInterface.Start()
 }
